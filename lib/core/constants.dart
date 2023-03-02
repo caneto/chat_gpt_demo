e80b7f649e96b808191e67d5ce1ae0aafa5a9c59
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-03-02 10:07:12 -0300

    Demo do ChatGPT

---
+++ lib/core/constants.dart
@@ -0,0 +1,4 @@
+const  String chatapiKey="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";
+
+
+const String baseURL="https://api.openai.com/v1";
---
+++ lib/features/global/provider/provider.dart
@@ -0,0 +1,8 @@
+import 'package:chat_gpt_demo/core/constants.dart';
+
+String endPoint(String endPoint) => "$baseURL/$endPoint";
+
+Map<String, String> headerBearerOption(String token) => {
+  "Content-Type": "application/json",
+  'Authorization': 'Bearer $token',
+};
